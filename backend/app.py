262e1d5c55c243cef8b357308575951f017cb762
sofietheresa <sofl3905@gmail.com>
2022-07-07 11:01:29 +0200

    finalised everything  ヽ(^o^)ノ

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,10 @@
 import flask
 from flask_cors import CORS
-import json
 from flask import jsonify, request, redirect
 import psycopg2 
 import os
 
+# define app
 app = flask.Flask(__name__)
 CORS(app, support_credentials=True, resources={r"/*": {"origins": "*"}})
 app.config["DEBUG"] = True
@@ -16,38 +16,26 @@ try:
             'host': "localhost",
             'port': "5432",
             'password': "1909"}
-
     con = psycopg2.connect(**dbconn)
     cur = con.cursor()
 
 
-   #  GET: Fetch all employees from the database
+   # READ: Fetch all employees from the database
     @app.route('/')
     def fetch_all_employees():
         cur.execute('SELECT * FROM employees')
         rows = cur.fetchall()
-        print(rows)
         return jsonify(rows)
 
-
-    # GET: Fetch book by bookId from database
-    @app.route('/<int:id>')
-    def fetch_by_id(employee_id=None):
-        cur.execute(f'SELECT * FROM employees WHERE id = {id}')
-        rows = cur.fetchall()
-        print(rows)
-        return jsonify(rows)
-
-    # POST: Insert employees and add them to the db
+    # CREATE: Insert employees and add them to the db
     @app.route('/add-employee', methods=['GET', 'POST'])
     def add_employee():
         if request.method == 'POST':
             data = request.form.to_dict()
-            print(data)
             cur.execute("INSERT INTO employees (id, f_name, l_name, location_id) VALUES (%s, %s, %s, %s)",
                         (data['ID'],  data['employeeFName'], data['employeeLName'], data['location']))
             con.commit()
-            return redirect('http://localhost:3000', code="200")
+            return redirect('http://localhost:3000')
         else:
             return 'Form submission failed'
 
@@ -55,23 +43,19 @@ try:
     @app.route('/delete-employee', methods=['GET','POST'])
     def delete_by_id():
         employee = request.form.to_dict()
-        print(employee['ID'])
         cur.execute(
             "DELETE FROM employees WHERE id = %s" % (employee['ID']))
         con.commit()
-        return redirect('http://localhost:3000', code="200")
+        return redirect('http://localhost:3000')
 
-    # PUT: Update employee by employeeId from the database
+    #UPDATED: Update employee by employeeId from the database
     @app.route('/update-employee', methods=['GET','POST'])
     def update_by_id():
         newdata = request.form.to_dict()
         sql = """UPDATE employees SET f_name = %s, l_name = %s, location_id = %s WHERE id = %s;"""
         cur.execute(sql,(newdata["employeeFName"], newdata['employeeLName'], newdata['location'], newdata['ID']))
-        #     'UPDATE employees SET f_name = "%s", l_name = %s, location_id = %s WHERE id = %s;'
-        #   %(newdata["employeeFName"], newdata['employeeLName'], newdata['location'], newdata['ID']))
         con.commit()
-
-        return redirect('http://localhost:3000', code="200")
+        return redirect('http://localhost:3000')
 
 except:
     print('Error')
